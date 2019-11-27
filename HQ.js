69ca9fcf6e52ccfe94d97a5b2efcb259ccea6ef6
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-11-26 19:27:50 -0500

    Update HQ.js

---
+++ HQ.js
@@ -0,0 +1,8 @@
+window.alert("JS Working");
+
+function addTask() {
+	window.alert(pri1Text);
+	//var newTask = document.createElement("div");
+	//newTask.className = "priority1";
+	//document.getElementById("tasks").appendChild(newTask);
+}
