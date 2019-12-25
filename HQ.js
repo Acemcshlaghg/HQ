bf82277d87d655c377d0163a1a487a901b03e24d
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-12-25 15:44:41 -0500

    Firebase added

--- HQ.js
+++ HQ.js
@@ -1,8 +1,23 @@
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var fbRef = database.ref();
+var tasks = fbRef.child("Tasks");
+
+for (var key in Object.keys(tasks)) {
+	document.write(key);
+}
 window.alert("JS Working");
 
 function addTask() {
-	window.alert(pri1Text);
-	//var newTask = document.createElement("div");
-	//newTask.className = "priority1";
-	//document.getElementById("tasks").appendChild(newTask);
+	var task = {
+		name: document.getElementById("textInput").value,
+		url: "https://www.google.com/"
+	};
+	var taskName = tasks.child(task.name).set(task.url);
+	document.getElementById("textInput").value = "";
+	/*var newTask = document.createElement("a");
+	newTask.innerHTML = task;
+	newTask.href = "https://www.google.com/";
+	newTask.target = "blank";
+	document.getElementById("tasks").appendChild(newTask);*/
 }
