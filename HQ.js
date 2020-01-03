fae109474139e3b871cb8706e5cdb8e3831d48d3
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2020-01-03 06:50:26 -0500

    Color Scheme

--- HQ.js
+++ HQ.js
@@ -1,23 +1,33 @@
 firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-var fbRef = database.ref();
-var tasks = fbRef.child("Tasks");
 
-for (var key in Object.keys(tasks)) {
-	document.write(key);
-}
+var tasks = firebase.database().ref().child("Tasks");
+
+tasks.once("value", function(snapshot) {
+	snapshot.forEach(function(child) {
+		var currentTask = document.createElement("a");
+		currentTask.innerHTML = child.key;
+		currentTask.href = "https://www.google.com/";
+		currentTask.target = "blank";
+		document.getElementById("tasks").appendChild(currentTask);
+		console.log(child.key);
+	});
+});
 window.alert("JS Working");
 
 function addTask() {
-	var task = {
-		name: document.getElementById("textInput").value,
-		url: "https://www.google.com/"
-	};
-	var taskName = tasks.child(task.name).set(task.url);
-	document.getElementById("textInput").value = "";
-	/*var newTask = document.createElement("a");
-	newTask.innerHTML = task;
-	newTask.href = "https://www.google.com/";
-	newTask.target = "blank";
-	document.getElementById("tasks").appendChild(newTask);*/
+	var taskName = document.getElementById("textInput").value; //text in input box
+	if (!(taskName === "")) {
+		var taskInfo = {
+			url: "https://www.google.com/",
+			thirdVal: "thirdVal"
+		};
+		tasks.child(taskName).set(taskInfo);
+
+		document.getElementById("textInput").value = "";
+		var newTask = document.createElement("a");
+		newTask.innerHTML = taskName;
+		newTask.href = "https://www.google.com/";
+		newTask.target = "blank";
+		document.getElementById("tasks").appendChild(newTask);
+	}
 }
